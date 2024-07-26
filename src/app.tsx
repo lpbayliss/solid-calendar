ae9d8c381c878efea6b1a0c22ee91750818f9f85
Luke Bayliss <hello@lukebayliss.com>
2024-07-26 22:01:22 +1000

    adds tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -3,15 +3,15 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
+import Nav from "./components/Nav";
 
 export default function App() {
   return (
     <Router
-      root={props => (
+      root={(props) => (
         <MetaProvider>
           <Title>SolidStart - Basic</Title>
-          <a href="/">Index</a>
-          <a href="/about">About</a>
+          <Nav />
           <Suspense>{props.children}</Suspense>
         </MetaProvider>
       )}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,21 @@ import Counter from "~/components/Counter";
 import { api } from "~/lib/api";
 
 export default function Home() {
-  const hello = createAsync(() => api.example.hello.query("world"));
+  const hello = createAsync(() => api.example.hello.query("Calendar"));
   return (
-    <main>
+    <main class="text-center mx-auto text-gray-700 p-4">
       <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
+        Hello world!
+      </h1>
       <Counter />
-      <p>
+      <p class="mt-8">
         Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
+        <a
+          href="https://start.solidjs.com"
+          target="_blank"
+          class="text-sky-600 hover:underline"
+        >
           start.solidjs.com
         </a>{" "}
         to learn how to build SolidStart apps.
