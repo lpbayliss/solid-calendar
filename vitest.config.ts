e97fdde87f92d16fef4c0e2ea6ca4ad8f9090a62
Luke Bayliss <hello@lukebayliss.com>
2024-07-26 23:46:35 +1000

    adds reporter for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,15 @@
-import solid from 'vite-plugin-solid';
-import { defineConfig } from 'vitest/config';
+import solid from "vite-plugin-solid";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [solid()],
   resolve: {
-    conditions: ['development', 'browser'],
+    conditions: ["development", "browser"],
+  },
+  test: {
+    coverage: {
+      reporter: ["text", "json-summary", "json"],
+      reportOnFailure: true,
+    },
   },
 });
